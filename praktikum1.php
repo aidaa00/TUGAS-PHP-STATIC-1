8e655a72e9cdf99717ef8a6a84b8cb512cdcfb5c
Sari' Sadida <sarisadida001@gmail.com>
2026-04-22 15:27:01 +0700

    Update Praktikum 1 PHP

--- praktikum1.php
+++ praktikum1.php
@@ -8,14 +8,24 @@ class Pengunjung {
         self::$jumlah++;
     }
 
+    public static function reset() {
+        self::$jumlah = 0;
+    }
+
 }
 
+// Membuat beberapa objek
 $p1 = new Pengunjung();
 $p2 = new Pengunjung();
 $p3 = new Pengunjung();
 $p4 = new Pengunjung();
 $p5 = new Pengunjung();
 
-echo "Jumlah Pengunjung: " . Pengunjung::$jumlah;
+echo "Jumlah Pengunjung awal: " . Pengunjung::$jumlah . "<br>"; // Output: 3
+
+// Memanggil method reset
+Pengunjung::reset();
+
+echo "Jumlah Pengunjung setelah reset: " . Pengunjung::$jumlah; // Output: 0
 
 ?>
